33dbd9f95a43d588cc6bab56ba80e7a64be69cd8
Bekzod Buyukov <bek.buyukov@gmail.com>
2021-02-11 15:43:30 +0500

    Small improvement

--- django_project/pages/views.py
+++ django_project/pages/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def show_home_page(request):
-    return HttpResponse("PostgreSQL-experience app is working!")
+    return HttpResponse('<h3>PostgreSQL-experience app is working!</h3>')
